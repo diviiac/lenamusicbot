078b0c1f298e8c351d60c3a5e7d91114ccbba25f
Mukesh Solanki <92496454+BotsClub@users.noreply.github.com>
2022-01-10 11:53:21 +0530

    Update __init__.py

--- AvengerMusic/__init__.py
+++ AvengerMusic/__init__.py
@@ -232,9 +232,9 @@ async def initiate_bot():
             else:
                    origin = repo.create_remote("origin", UPSTREAM_REPO)
             origin.fetch()
-            repo.create_head(UPSTREAM_BRANCH, origin.refs.master)
-            repo.heads.master.set_tracking_branch(origin.refs.master)
-            repo.heads.master.checkout(True)
+            repo.create_head(UPSTREAM_BRANCH, origin.refs.main)
+            repo.heads.main.set_tracking_branch(origin.refs.main)
+            repo.heads.main.checkout(True)
             try:
                 repo.create_remote("origin", UPSTREAM_REPO)
             except BaseException:
